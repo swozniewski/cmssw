5c7d5baa1107694a8fcfadd06710bcfcb82e1aef
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2019-07-15 17:58:21 +0200

    install new dataformat in anti muon discriminator

--- RecoTauTag/Configuration/python/HPSPFTaus_cff.py
+++ RecoTauTag/Configuration/python/HPSPFTaus_cff.py
@@ -322,7 +322,6 @@ hpsPFTauDiscriminationByMVA6ElectronRejection = recoTauDiscriminantCutMultiplexe
     PFTauProducer = cms.InputTag('hpsPFTauProducer'),
     Prediscriminants = requireDecayMode.clone(),
     toMultiplex = cms.InputTag('hpsPFTauDiscriminationByMVA6rawElectronRejection'),
-    key = cms.InputTag('hpsPFTauDiscriminationByMVA6rawElectronRejection:category'),
     loadMVAfromDB = cms.bool(True),
     mapping = cms.VPSet(
         cms.PSet(
